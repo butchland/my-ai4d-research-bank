1d1a70c89737553af25007752783aef04875d06e
butchland <butchland@users.noreply.github.com>
2023-05-29 10:05:13 +0000

    deploy: b27f7a5c48b2222fbc459323a77db7045ce2f48b

--- src/constants/texts.ts
+++
@@ -1,4 +0,0 @@
-const MAP_PREVIEW_NOTE =
-	'Note: The following visualization represents the geographical scope of the dataset.'
-
-export default MAP_PREVIEW_NOTE
--- src/pages/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as CatalogueItemPage } from './CatalogueItemPage'
-export { default as CataloguePage } from './CataloguePage'
-export { default as ContributionPage } from './ContributionPage'
-export { default as LandingPage } from './LandingPage'
